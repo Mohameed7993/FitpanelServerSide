6ae4ae5c918a463fe2b788641b447f865dc2141a
Mohameed7993 <hmody.chechareto@gmail.com>
2025-07-29 23:39:32 +0300

    Update: finished Firebase env config and final fixes

--- admin.js
+++ admin.js
@@ -1,6 +1,10 @@
 // admin.js
 const admin = require('firebase-admin');
-const serviceAccount = require('./serviceAccountKey.json'); // download from Firebase Console
+require('dotenv').config();
+
+const serviceAccount = JSON.parse(
+  Buffer.from(process.env.GOOGLE_CREDENTIALS_BASE64, 'base64').toString('utf-8')
+);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
